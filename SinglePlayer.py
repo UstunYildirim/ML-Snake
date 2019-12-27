d3332bfe87c5a9fd4cfe17f03367ea10b8cd6f66
Ustun Yildirim <ustun@mailbox.org>
2019-12-27 15:56:41 -0500

    Single player support geldi :)

---
+++ SinglePlayer.py
@@ -0,0 +1,40 @@
+import numpy as np 
+from DataHandler import *
+from Game import *
+from Visualize import *
+
+class SinglePlayer():
+    
+    def __init__(s, m,n):
+        s.m = m
+        s.n = n
+        s.game = Game(m,n)
+
+    def Play(s):
+        g = s.game
+        vsr = Visualize(game=g)
+        while True:
+            vsr.display(g)
+            print("Score", g.score)
+            print("Turns", g.numTurns)
+            if g.dead == 1:
+                print("Game over!")
+                return
+            if g.won == 1:
+                print("You win!")
+                return
+
+            i = input("")
+            if i == 'j':
+                g.snakeDir = g.down
+            elif i == 'k':
+                g.snakeDir = g.up
+            elif i == 'h':
+                g.snakeDir = g.left
+            elif i == 'l':
+                g.snakeDir = g.right
+            elif i == 'q':
+                print('Bye!')
+                return
+            g.timeStep()
+            vsr.goBackNLines(s.m+3)
